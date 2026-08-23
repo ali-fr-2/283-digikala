0695496dfc92c1bea37e25a47b370bacb2d2f5f6
ali-fr-2 <alifarjamy2@gmail.com>
2026-08-23 12:52:45 +0330

    284-master-blade-panel

---
+++ resources/views/Admin/layout/head-tag.blade.php
@@ -0,0 +1,20 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
+    <title>Sales Admin | CORK الگوی مدیریتی تمام ریسپانسیو </title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('AdminAssets/assets/img/favicon.ico') }}"/>
+    <link href="{{ asset('AdminAssets/assets/css/loader.css') }}" rel="stylesheet" type="text/css" />
+    <script src="{{ asset('AdminAssets/assets/js/loader.js') }}"></script>
+    <!-- BEGIN GLOBAL MANDATORY STYLES -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
+    <link href="{{ asset('AdminAssets/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('AdminAssets/assets/css/plugins.css') }}" rel="stylesheet" type="text/css" />
+    <!-- END GLOBAL MANDATORY STYLES -->
+
+    <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM STYLES -->
+    <link href="{{ asset('AdminAssets/plugins/apex/apexcharts.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('AdminAssets/assets/css/dashboard/dash_1.css') }}" rel="stylesheet" type="text/css" />
+    <!-- END PAGE LEVEL PLUGINS/CUSTOM STYLES -->
+
+</head>
---
+++ resources/views/Admin/layout/master.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+
+@include('admin.layout.head-tag')
+
+<body>
+
+    @include('admin.layout.loader')
+    @include('admin.layout.navbar-part-one')
+    @include('admin.layout.navbar-part-two')
+    <div class="main-container" id="container">
+        <div class="overlay"></div>
+        <div class="search-overlay"></div>
+
+
+        @include('admin.layout.sidebar')
+
+        <div id="content" class="main-content">
+            <div class="layout-px-spacing">
+
+                    @include('admin.layout.footer')
+            </div>
+        </div>
+
+        @include('admin.layout.js')
+    </div>
+
+</body>
+
+
+
+</html>
